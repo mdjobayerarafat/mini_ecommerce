12dd724f3f7d2bd2ee27d4762a228c4359d5be8b
mdjobayerarafat <mdjobayerarafat@gmail.com>
2024-09-22 21:03:34 +0600

    start

---
+++ mini_ecommerce/src/main/java/com/mdjobayerarafat/mini_ecommerce/MiniEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.mdjobayerarafat.mini_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniEcommerceApplication.class, args);
+	}
+
+}
---
+++ mini_ecommerce/src/test/java/com/mdjobayerarafat/mini_ecommerce/MiniEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdjobayerarafat.mini_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
